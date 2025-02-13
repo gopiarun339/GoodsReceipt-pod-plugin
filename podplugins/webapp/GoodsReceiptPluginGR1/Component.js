5feebc948ba0a80329000fccb32e1bc4925f6f62
Arun GK <gopi.arun@stellium.com>
2025-02-13 11:00:35 +0000

    GIGR Plugin

---
+++ podplugins/webapp/GoodsReceiptPluginGR1/Component.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/dm/dme/podfoundation/component/production/ProductionUIComponent"
+], function(ProductionUIComponent) {
+    "use strict";
+
+    /**
+     * 
+     */
+    var Component = ProductionUIComponent.extend("Arun.ext.viewplugins.GoodsReceiptPluginGR1.Component", {
+        metadata : {
+            manifest : "json"
+        }
+    });
+
+    return Component;
+});
